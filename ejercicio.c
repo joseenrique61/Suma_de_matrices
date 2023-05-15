ad47013779e14dfa3435beed706284f6d8124d1c
joseenrique61 <joseenrique61@hotmail.com>
2023-05-15 16:15:01 -0500

    Ejercicio finalizado

--- ejercicio.c
+++ ejercicio.c
@@ -3,12 +3,15 @@
 
 int main()
 {
+    // Ponemos la semilla para el número aleatorio
     srand(time(NULL));
 
+    // Inicializamos las variables
     int x[3][2] = {{0, 0}, {0, 0}, {0, 0}};
     int y[3][2] = {{0, 0}, {0, 0}, {0, 0}};
     int z[3][2] = {{0, 0}, {0, 0}, {0, 0}};
 
+    // Ponemos números aleatorios a cada elemento de las matrices "x" y "y" y los sumamos en "z"
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 2; j++)
@@ -19,5 +22,13 @@ int main()
         }
     }
 
+    // Impimimos la matriz
     for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            printf("%i ", z[i][j]);
+        }
+        printf("\n");
+    }
 }
